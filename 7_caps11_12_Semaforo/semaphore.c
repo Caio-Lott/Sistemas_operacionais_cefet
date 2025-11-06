cbe8c601df5779d8fa1fe64f03431dbe6a149423
Henrique-R-Lima <rick.rlima@hotmail.com>
2025-11-05 21:23:45 -0300

    7 semaphore doesnt work

--- 7_caps11_12_Semaforo/semaphore.c
+++ 7_caps11_12_Semaforo/semaphore.c
@@ -1,52 +1,112 @@
+// gcc -lrt -lpthread semaphore.c -o s && ./s
+
 #include <semaphore.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
 
-#define NUM_PROCESSES      100
+#define SHM_NAME        "sum"
+#define NUM_PROCESSES      3
 #define NUM_OPERATIONS  100000
 
-int sum = 0;
 sem_t sem;
+
+double * createSharedMemory(int num_proc){
+    // Configuration parameters
+    int shm_fd;
+    size_t shm_size = sizeof(double); // Tamanho exato para N resultados
+    // Create the shared memory object
+    shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed");
+        exit(1); 
+    }
+    // Define segment size
+    if (ftruncate(shm_fd, shm_size) == -1) {
+        perror("ftruncate failed");
+        shm_unlink(SHM_NAME); // Try to clear memory
+        exit(1); 
+    }
+    // Pointer to the mapping
+    double *sm_pointer;
+    sm_pointer = mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (sm_pointer == MAP_FAILED) {
+        perror("mmap failed");
+        shm_unlink(SHM_NAME); // Try to clear memory
+        exit(1);
+    }
+
+    // Close file descriptor, but the mapping stays
+    close(shm_fd); 
+    printf("Created shared memory successfully!\n");
+    return sm_pointer;
+}
+
+void createProcesses(int * f_val, int * proc_idx){
+    // Process creation
+    *f_val = 1;
+    *proc_idx = 0;
+    
+    while(*f_val > 0 && *proc_idx < NUM_PROCESSES){
+        *f_val = fork();
+        if(*f_val < 0){
+            perror ("Error while creating process") ;
+            exit(1); 
+        }
+        *proc_idx = *proc_idx + 1;
+        if(*f_val == 0){
+            printf("Child %i created\n", *proc_idx);
+        }
+    }
+}
+
+void childWork(double * sum, int * proc_idx){
+    for(int i=0; i < NUM_OPERATIONS; i++){
+        sem_wait(&sem); // down
+        *sum = *sum + 1;
+        sem_post(&sem); // up
+    }
+    // printf("%i done! last value: %.0f\n", *proc_idx, *sum);
+    printf("%i done", *proc_idx);
+    exit(0);
+}
+
+void parentWork(double * sum){
+    for(int i=0; i < NUM_PROCESSES; i++){
+        wait(0);
+    }
+    printf("Expected %i, got %.0f\n", NUM_PROCESSES * NUM_OPERATIONS, *sum);
+}
 
 int main() {
 
-    int id = 0;
-    int status = 0;
+    // Create shared memory
+    double *sum = createSharedMemory(NUM_PROCESSES);
+    *sum = 0;
 
     // Initialize semaphore
-    if (sem_init(&sem, 1, 1) != 0) {
+    if(sem_init(&sem, 1, 1) != 0){
         perror("Semaphore initialization failed");
-        return 1;
+        exit(1);
     }
 
     // Create processes
-    for(int i=0; i < NUM_PROCESSES && id > 0; i++){
-        id = fork();
-    }
-
+    int proc_idx;
+    int f_val;
+    createProcesses(&f_val, &proc_idx);
     
     // Child work
-    if(id==0){
-        for(int i=0; i < NUM_OPERATIONS; i++){
-            sem_wait(&sem); // down
-            sum++;
-            sem_post(&sem); // up
-        }
-        printf("%i done!\n", id);
-        return 0;
+    if(f_val==0){
+        childWork(sum, &proc_idx);
     }
     
     // Parent work
-    if(id>0){
-        printf("Pai vivo");
-        for(int i=0; i < NUM_PROCESSES; i++){
-            wait(&status);
-            if(!WIFEXITED(status)){
-                printf("Process terminated with error\n");
-            }
-        }
-        printf("Expected %i, got %i\n", NUM_PROCESSES * NUM_OPERATIONS, sum);
+    if(f_val>0){
+        parentWork(sum);
     }
     return 0;
 }
